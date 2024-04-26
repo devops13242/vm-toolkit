e3a199b646ba975003710c4945597a510825b267
devops13242 <devops13242@users.noreply.github.com>
2024-04-26 10:42:30 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='vmtoolkit',
+      version='0.0.1',
+      description='vm toolkit',
+      url='https://github.com/devops13242/vmtoolkit',
+      author='devops13242',
+      author_email='devops13242@users.noreply.github.com',
+      license='apache2',
+      packages=['vmtoolkit'],
+      zip_safe=False)
